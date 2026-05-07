1db252f5e14977808a34c0e8c051365394a8e7b0
muxammadsodiq <muxammadsodiq2703@gmail.com>
2026-05-07 19:21:44 +0500

    commiiiiit

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -1,5 +1,5 @@
 
-export const supabaseUrl = "https://qxdgubrnxasnzbsydasf.supabase.co/rest/v1/";
+export const supabaseUrl = "https://qxdgubrnxasnzbsydasf.supabase.co";
 export const publicAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4ZGd1YnJueGFzbnpic3lkYXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5ODU4NDUsImV4cCI6MjA5MzU2MTg0NX0.as0rrFGUrm4pqLl4FHJpzBqwTKiCXFiB_pvfLuGzbeI";
 export const projectId = supabaseUrl.match(/^https:\/\/([^.]+)\.supabase\.co/i)?.[1] || "";
 
